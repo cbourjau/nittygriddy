094d927c4207a40aa70782e9bb65efbd69f76158
cbourjau <christian.bourjau@cern.ch>
2017-10-30 16:32:25 +0100

    added pyyaml as a dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from glob import glob
 
-install_requires = ['rootpy', 'pygments']
+install_requires = ['rootpy', 'pygments', 'pyyaml']
 tests_require = ['nose']
 
 setup(
