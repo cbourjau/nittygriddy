894d5071ab979f09b077080e06f88c2989034b62
cbourjau <christian.bourjau@cern.ch>
2017-12-08 12:24:15 +0100

    bumped version 1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ tests_require = ['nose']
 
 setup(
     name='nittygriddy',
-    version='1.0.4',
+    version='1.0.5',
     description="Convinient way to deploy your ALICE analysis locally (sequential and proof lite) or on the grid",
     author='Christian Bourjau',
     author_email='christian.bourjau@cern.ch',
